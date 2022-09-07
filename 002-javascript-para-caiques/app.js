1ed5e052f90c39eca68e6488ac8fdfce577f91a3
Rodrigo Barros <rodrigorgrb@gmail.com>
2022-09-06 23:01:18 -0300

    aula 002

---
+++ 002-javascript-para-caiques/app.js
@@ -0,0 +1,49 @@
+//console.log("Hello World Caique viadao");
+
+//variaveis
+// let - variaveis, const constantes
+// concatenar
+// let nome = prompt("Qual o seu nome?");
+// let sobrenome = prompt("Qual o seu sobrenome?");
+// console.log(`Seja bem vindo ${nome} ${sobrenome}`);
+
+//null = vazio
+// let numero1 = null;
+// let numero2 = 'null';
+// let numero3 = '';
+
+// let conta = 0;
+// let valor = prompt('Quanto deseja depositar?');
+
+// console.log(`Você depositou ${valor}`);
+
+// let conta = 20;
+// let valor = parseInt(prompt("Quanto deseja depositar?"));
+// conta = conta + valor;
+
+// console.log(`Sua conta possui R$ ${conta}`);
+
+// let numero1 = parseInt(prompt("digite o numero 1"));
+// let numero2 = parseInt(prompt("digite o numero 2"));
+// let resultado = numero1 + numero2;
+// console.log(resultado);
+
+// let metro = parseInt(prompt("Digite um numero em metros"));
+// let resultado = metro * 1000;
+// console.log(resultado);
+
+// let pizza = 1
+// let pedaco = pizza / 8
+// parseFloat
+// let salario = parseFloat(prompt("digite o seu salario"));
+// let porcentagem = parseFloat(prompt("digite a porcentagem do aumento"));
+// let aumento = (salario * porcentagem) / 100;
+// console.log(`Seu aumento foi de: R$ ${aumento},00 então seu
+// salario final é: R$${salario + aumento},00`);
+let idade = parseInt(prompt("digite a idade"));
+
+if (idade >= 18) {
+  console.log("É maior de idade");
+} else {
+  console.log("Não é maior de idade");
+}
